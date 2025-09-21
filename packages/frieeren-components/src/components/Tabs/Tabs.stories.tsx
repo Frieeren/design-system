6b2761b7a4fb41f2ced4e9a24a8736c7cb7c4709
Jeong Seungdeok <32917014+seungdeok@users.noreply.github.com>
2025-09-21 11:59:52 +0900

    [Feat/#66] Tabs 컴포넌트 리팩토링 (#71)
    * chore(@team-frieeren/components): @team-frieeren/icon 의존 패키지를 위한 install_command 수정
    
    * feat(@team-frieeren/components): Tabs 컴포넌트 리팩토링
    
    * refactor(@team-frieeren/components): tabs dir props 제거
    
    * refactor(@team-frieeren/components): tabs label style 추가
    
    * refactor(@team-frieeren/components): Tabs style 수정

--- packages/frieeren-components/src/components/Tabs/Tabs.stories.tsx
+++ packages/frieeren-components/src/components/Tabs/Tabs.stories.tsx
@@ -1,13 +1,59 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { useState } from "react";
 import { Tabs } from "./Tabs";
+
+const tabItems = [
+  {
+    value: "일일 업무",
+    label: (
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "6px" }}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <rect width="24" height="24" transform="translate(0.5 0.5)" fill="white" />
+          <path
+            d="M10.4663 15.0813L16.8226 8.725C16.9726 8.575 17.1476 8.5 17.3476 8.5C17.5476 8.5 17.7226 8.575 17.8726 8.725C18.0226 8.875 18.0976 9.05325 18.0976 9.25975C18.0976 9.46625 18.0226 9.64425 17.8726 9.79375L10.9913 16.6938C10.8413 16.8438 10.6663 16.9188 10.4663 16.9188C10.2663 16.9188 10.0913 16.8438 9.94135 16.6938L6.71635 13.4688C6.56635 13.3188 6.49435 13.1408 6.50035 12.9348C6.50635 12.7288 6.5846 12.5505 6.7351 12.4C6.8856 12.2495 7.06385 12.1745 7.26985 12.175C7.47585 12.1755 7.65385 12.2505 7.80385 12.4L10.4663 15.0813Z"
+            fill="currentColor"
+          />
+        </svg>
+        <div>일일 업무</div>
+      </div>
+    )
+  },
+  {
+    value: "회의 일정",
+    label: (
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "6px" }}>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="24" height="24" fill="white" />
+          <circle cx="12.7324" cy="12" r="6.5" fill="white" stroke="currentColor" />
+          <rect width="8" height="2" transform="translate(8.73242 11)" fill="white" />
+          <circle cx="9.73242" cy="12" r="1" fill="currentColor" />
+          <circle cx="12.7324" cy="12" r="1" fill="currentColor" />
+          <circle cx="15.7324" cy="12" r="1" fill="currentColor" />
+          <path d="M6.73205 15L9.23193 18H5L6.73205 15Z" fill="#B7C2D0" />
+        </svg>
+        <div>회의 일정</div>
+      </div>
+    )
+  }
+];
 
 /**
  * **radix docs**
  * https://www.radix-ui.com/primitives/docs/components/tabs
  */
 const meta = {
-  title: "Components/Tabs",
+  title: "v2/Components/Tabs",
   component: Tabs,
   parameters: {
     layout: "centered"
@@ -16,21 +62,17 @@ const meta = {
   argTypes: {
     value: {
       control: "select",
-      options: ["tab1", "tab2", "tab3"],
+      options: ["일일 업무", "회의 일정"],
       description: "활성화 할 value"
     },
-    onValueChange: {
-      control: "object",
-      description: "value 이벤트 핸들러"
-    },
-    dir: {
+    tabItems: {
       control: "select",
-      options: ["ltr", "rtl"],
-      description: "tab의 읽기 방향을 지정"
+      options: tabItems,
+      description: "tab의 아이템 리스트"
     },
     defaultValue: {
       control: "select",
-      options: ["tab1", "tab2", "tab3"],
+      options: ["일일 업무", "회의 일정"],
       description: "초기 활성 tab 값을 설정"
     }
   }
@@ -39,40 +81,11 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Base: Story = {
   args: {
     width: "360px",
-    tabItems: [
-      { value: "tab1", text: "Tab1", content: <div></div> },
-      { value: "tab2", text: "Tab2", content: <div></div> },
-      { value: "tab3", text: "Tab3", content: <div></div> }
-    ],
-    defaultValue: "tab1",
-    dir: "ltr"
+    tabItems,
+    defaultValue: "일일 업무"
   },
-  render: args => <Tabs {...args} key={`tabs_${args.dir}_${args.defaultValue}`} />
+  render: args => <Tabs {...args} key={`tabs_${args.defaultValue}`} />
 };
-
-export function ControlledTabs() {
-  const [value, setValue] = useState<string>("tab1");
-
-  const handleChangeTab = (value: string) => {
-    setValue(value);
-    console.log("Active Tab: ", value);
-  };
-
-  return (
-    <div style={{ width: "calc(100vw * 0.5)", minWidth: 320, maxWidth: 540 }}>
-      <Tabs
-        tabItems={[
-          { value: "tab1", text: "Tab1", content: <div>Tab1 Content</div> },
-          { value: "tab2", text: "Tab2", content: <div>Tab2 Content</div> },
-          { value: "tab3", text: "Tab3", content: <div>Tab3 Content</div> }
-        ]}
-        defaultValue="tab1"
-        value={value}
-        onValueChange={handleChangeTab}
-      />
-    </div>
-  );
-}
--- packages/frieeren-components/src/components/Tabs/Tabs.tsx
+++ packages/frieeren-components/src/components/Tabs/Tabs.tsx
@@ -1,32 +1,32 @@
 import { useRef } from "react";
+import * as TabsBase from "@radix-ui/react-tabs";
 import { TabsProps } from "./Tabs.type";
-import { List, Root, Trigger, Content } from "./TabsBase";
 import { useTabsIndicator } from "./useTabsIndicator";
 
-const Tabs: React.FC<TabsProps> = props => {
+export const Tabs = (props: TabsProps) => {
   const { width = "100%", tabItems, value, ...rest } = props;
   const listRef = useRef<HTMLDivElement>(null);
   const { indicatorStyle } = useTabsIndicator({ listRef });
 
   return (
-    <Root value={value} {...rest} style={{ width }}>
-      <div ref={listRef} className="tabs--list-wrapper">
-        <List data-orientation="horizontal">
-          {tabItems.map(item => (
-            <Trigger key={`tabTrigger_${item.value}`} value={item.value}>
-              {item.text}
-            </Trigger>
-          ))}
-        </List>
-        <div className="tabs--indicator" style={indicatorStyle} />
-      </div>
-      {tabItems.map(item => (
-        <Content key={`tabContent_${item.value}`} value={item.value}>
-          {item.content}
-        </Content>
-      ))}
-    </Root>
+    <TabsBase.Root
+      data-frieeren-component="Tabs"
+      className="tabs"
+      value={value}
+      {...rest}
+      style={{ width }}
+    >
+      <TabsBase.List ref={listRef} data-orientation="horizontal" className="tabs--list">
+        {tabItems.map(item => (
+          <TabsBase.Trigger
+            key={`tabTrigger_${item.value}`}
+            value={item.value}
+            className="tabs--trigger"
+          >
+            {item.label}
+          </TabsBase.Trigger>
+        ))}
+      </TabsBase.List>
+    </TabsBase.Root>
   );
 };
-
-export { Tabs };
